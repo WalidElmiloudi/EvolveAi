6e7a97f3df6d3e6f61a6c3731b65150b64a53428
WalidElmiloudi <elwalid38@gmail.com>
2026-01-20 21:01:56 +0100

    feat: add example configuration file for database connection

---
+++ app/Config/config.exemple.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // Database driver (pgsql, mysql, sqlite, ...)
+    'PDO_driver' => 'pgsql',
+
+    // Database host
+    'host'       => 'localhost',
+
+    // Database port
+    'port'       => 5432,
+
+    // Database name
+    'dbname'     => 'database_name',
+
+    // Database username
+    'username'   => 'db_user',
+
+    // Database password
+    'password'   => 'db_password',
+];

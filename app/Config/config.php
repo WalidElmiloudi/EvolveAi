f414cd640bc04e9c5f421327554246ee0e164e17
WalidElmiloudi <elwalid38@gmail.com>
2026-01-20 12:24:34 +0100

    checking the gitignore

--- app/Config/config.php
+++ app/Config/config.php
@@ -3,7 +3,7 @@
 return [
   'PDO_driver' => 'pgsql',
   'host'       => 'localhost',
-  'port'       => 5432,
+  'port'       => '5432',
   'dbname'     => 'evolve_ai',
   'username'   => 'postgres',
   'password'   => 'Root1234',

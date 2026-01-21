9f683bfec8a22a5a4aed3ace915b627334b282cd
imadprogram <kimad7141@gmail.com>
2026-01-21 09:47:57 +0100

    added prompt ready to send to the ai

---
+++ app/Repositories/PlanRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Core\Database;
+
+
+class PlanRepository {
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getInstance();
+    }
+
+    public function dailyPlan($user_id , $goal_summary){
+        $sql = "INSERT INTO daily_plan(user_id , goal_summary , completion_rate)
+                VALUES(:user_id , :goal_summary , 0)
+                RETURNING id";
+
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->execute([
+            'user_id' => $user_id,
+            'goal_summary' => $goal_summary
+        ]);
+
+        $result = $stmt->fetch();
+
+        return $result['id'];
+    }
+
+    public function saveTasks($plan_id , $tasks) {
+        $sql = "INSERT INTO task (plan_id, title, description, duration, priority, is_skill_validation) 
+                VALUES (:pid, :title, :desc, :dur, :prio, :is_val)";
+
+        $stmt = $this->db->prepare($sql);
+
+        foreach($tasks as $task){
+
+            $stmt->execute([
+                'pid' => $plan_id,
+                'title' => $task->title,
+                'desc' => $task->description,
+                'dur' => $task->duration,
+                'prio' => $task->priority,
+                'is_val' => 0
+            ]);
+        }
+    }
+
+}
